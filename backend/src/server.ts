63e0e2fef625998c481b1a3e70ef384ef22f3d88
Akmal <akmalajmal2@gmail.com>
2025-11-23 13:35:41 +0530

    frontend api file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ app.use(morgan("tiny"));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tiny-link-lfbc.vercel.app"],
+    origin: ["http://localhost:5173", "https://tiny-link-bjp8.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
